b5648b694fe8579425f679175e37039722016cb3
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2024-09-16 08:36:00 +0200

    sd v17

--- shoes_dealer/models/stock_move.py
+++ shoes_dealer/models/stock_move.py
@@ -7,11 +7,15 @@ class StockMove(models.Model):
     _inherit = "stock.move"
 
     # Comercialmente en cada Albaran quieren saber cuántos pares se han vendido:
-    @api.depends("product_id", "quantity_done")
-    def _get_shoes_stock_move_pair_count(self):
-        for record in self:
-            record["pairs_count"] = record.product_id.pairs_count * record.quantity_done
+# V16 (para borrar si funciona v17, porque ya no existe quantity_done):    @api.depends("product_id", "quantity_done")
+#    def _get_shoes_stock_move_pair_count(self):
+#        for record in self:
+#            record["pairs_count"] = record.product_id.pairs_count * record.quantity_done
 
+    @api.depends('move_line_ids.quantity_done')
+    def _get_shoes_stock_move_pair_count(self):
+        for move in self:
+            move.pairs_count = sum(line.quantity_done for line in move.move_line_ids) * self.product_id.pairs_count
     pairs_count = fields.Integer(
         "Pairs", store=True, compute="_get_shoes_stock_move_pair_count"
     )
