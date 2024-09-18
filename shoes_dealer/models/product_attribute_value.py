b1fc2992f4ff9f525cd7839fddb4bcb907f59a06
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2024-09-18 09:10:24 +0200

    sd v17

--- shoes_dealer/models/product_attribute_value.py
+++ shoes_dealer/models/product_attribute_value.py
@@ -2,6 +2,7 @@
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 
 from odoo import fields, models, api
+from odoo.exceptions import UserError
 
 class ProductAttributeValue(models.Model):
     _inherit = 'product.attribute.value'
@@ -16,3 +17,11 @@ class ProductAttributeValue(models.Model):
     set_hidden = fields.Boolean('Set hidden', store=False, compute='_get_set_hidden')
 
     material_id = fields.Many2one('product.material', string='Material')
+
+    @api.constrains('is_custom')
+    def _constrains_no_custom_in_sizes_colors(self):
+        for record in self:
+            color_attribute = self.env.company.color_attribute_id
+            size_attribute = self.env.company.size_attribute_id
+            if record.is_custom and record.attribute_id.id in [color_attribute.id, size_attribute.id]:
+                raise UserError('No se permiten valores personalizados en los valores de color y talla.')
