5145b5fe8c0e8cc63e758e191b44303a2426c240
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2024-09-18 12:50:11 +0200

    sd v17

--- shoes_dealer/models/product_attribute_custom_value.py
+++ shoes_dealer/models/product_attribute_custom_value.py
@@ -2,6 +2,7 @@
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 
 from odoo import fields, models, api
+from odoo.exceptions import UserError
 
 class ProductAttributeCustomValue(models.Model):
     _inherit = 'product.attribute.custom.value'
