b07490d73b740972c0c15892976eb3eb28f4240c
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2024-09-18 08:21:48 +0200

    sd v17

--- shoes_dealer/models/assortment_pair.py
+++ shoes_dealer/models/assortment_pair.py
@@ -12,7 +12,16 @@ class AssortmentPair(models.Model):
     product_id = fields.Many2one('product.product', string='Product variant')
     product_tmpl_id = fields.Many2one('product.template', string='Product template', related='product_id.product_tmpl_id')
     bom_qty = fields.Integer('Bom units')
-    qty = fields.Integer('Pairs')
+    qty = fields.Integer('Pairs', compute='_get_sml_qty')
     partner_id = fields.Many2one('res.partner', string='Partner')
     sml_id = fields.Many2one('stock.move.line', string='Stock move line')
     sm_id = fields.Many2one('stock.move', string='Stock move', related='sml_id.move_id')
+
+    @api.depends('sml_id.qty_done', 'product_id')
+    def _get_sml_qty(self):
+        for record in self:
+            factor = 1
+            if (record.x_sml_id.location_usage in ('internal', 'transit')) and (
+                    record.sml_id.location_dest_usage not in ('internal', 'transit')):
+                factor = -1
+            record['qty'] = record.bom_qty * record.sml_id.qty_done * factor
