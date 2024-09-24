910f66fd9fc3c3bd3bb3591120fe92ff6df3b01e
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2024-09-24 12:54:59 +0200

    sd v17

--- shoes_campaign/models/project_task.py
+++ shoes_campaign/models/project_task.py
@@ -10,7 +10,7 @@ class ProjectTask(models.Model):
 
     # Datos comunes para creación de productos desde tareas:
 
-    product_brand_id = fields.Many2one('product.brand', related='project_id.brand_id')
+    product_brand_id = fields.Many2one('product.brand', related='project_id.product_brand_id')
     manufacturer_id = fields.Many2one('res.partner', string='Manufacturer')
     gender = fields.Selection(
         [("man", "Man"), ("woman", "Woman"), ("unisex", "Unisex")],
