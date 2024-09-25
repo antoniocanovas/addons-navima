519b7ef3c4f3b81222b58fd552cac163e376112a
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2024-09-25 11:20:05 +0200

    sd v17

--- shoes_dealer/models/product_attribute_custom_value.py
+++ shoes_dealer/models/product_attribute_custom_value.py
@@ -7,5 +7,5 @@ class ProductAttributeCustomValue(models.Model):
     _inherit = 'product.attribute.custom.value'
 
     # Validación de datos custom introducidos pasa a líne de venta para evitar que el chequeo sea al guardar el pedido.
-    assortment_pair = fields.Char(related='sale_order_line_id.assortment_pair')
-    pairs_count = fields.Integer(related='sale_order_line_id.pairs_custom_assortment_count')
+#    assortment_pair = fields.Char(related='sale_order_line_id.assortment_pair')
+#    pairs_count = fields.Integer(related='sale_order_line_id.pairs_custom_assortment_count')
