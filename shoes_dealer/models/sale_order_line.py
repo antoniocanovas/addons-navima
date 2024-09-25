be22a36f7feff0580d5fc88603ae881ae5643b96
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2024-09-25 11:10:30 +0200

    sd v17

--- shoes_dealer/models/sale_order_line.py
+++ shoes_dealer/models/sale_order_line.py
@@ -26,7 +26,7 @@ class SaleOrderLine(models.Model):
     pairs_count = fields.Integer(
         "Pairs", store=True, compute="_get_shoes_sale_line_pair_count"
     )
-    pairs_custom_assortment_count = fields.Integer("Custom assortment pairs", readonly=True)
+    pairs_custom_assortment_count = fields.Integer("Custom assortment pairs")
     assortment_pair = fields.Char('Assortment pairs')
 
     # Precio especial del para en la línea de ventas, recalculará precio unitario del producto surtido:
