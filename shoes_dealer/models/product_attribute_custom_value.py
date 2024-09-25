a7a2fa009cd5d1d0a192e7d74b9f0fb0d7f4d98b
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2024-09-25 09:45:06 +0200

    sd v17

--- shoes_dealer/models/product_attribute_custom_value.py
+++ shoes_dealer/models/product_attribute_custom_value.py
@@ -6,59 +6,6 @@ from odoo import fields, models, api
 class ProductAttributeCustomValue(models.Model):
     _inherit = 'product.attribute.custom.value'
 
+    # Validación de datos custom introducidos pasa a líne de venta para evitar que el chequeo sea al guardar el pedido.
     assortment_pair = fields.Char(related='sale_order_line_id.assortment_pair')
     pairs_count = fields.Integer(related='sale_order_line_id.pairs_custom_assortment_count')
-
-
-    # Validación de datos introducidos pasada a líne de venta para evitar que el chequeo sea al guardar el pedido,
-    # aquí queda exclusivamente la grabación final del campo (que como es para imprimir podría quedar en SOL):
-    """
-    @api.depends('custom_value')
-    def _get_assortment_pair(self):
-        for record in self:
-            cleanvalues, sizes, pairs, pair_products, pairs_count = "", "", "", "", 0
-            size_attribute = self.env.company.size_attribute_id
-            sale_line_product = record.sale_order_line_id.product_id
-            sale_line_product_color = sale_line_product.color_attribute_id
-            shoes_pair_model = sale_line_product.product_tmpl_single_id
-
-            if sale_line_product.is_assortment:
-                # Quitar espacios del campo custom del surtido:
-                customvalue = record.custom_value.replace(" ", "").lower()
-                customvalues = customvalue.split(",")
-
-                # Chequear que las tallas o cantidades introducidas son válidas y el par está creado:
-                for li in customvalues:
-                    element = li.split("x")
-                    # Para tallas (encontrar si existe la talla y color en el par):
-                    color_attribute_value_id = sale_line_product_color
-                    size_attribute_value_id = self.env['product.attribute.value'].search(
-                        [('attribute_id', '=', size_attribute.id), ('name', '=', element[0])])
-                    if not size_attribute_value_id.id:
-                        raise UserError("La talla " + str(element[0]) + " no existe en el sistema.")
-
-                    pppair = self.env['product.product'].search([('color_attribute_id', '=', color_attribute_value_id.id),
-                                                                 ('size_attribute_id', '=', size_attribute_value_id.id),
-                                                                 ('product_tmpl_id', '=', shoes_pair_model.id)])
-                    if not pppair.id:
-                        raise UserError("No encuentro el par suelto de talla " + str(
-                            element[0]) + " y color " + color_attribute_value_id.name + " en este modelo.")
-
-                    # Para cantidades (ok):
-                    try:
-                        qty = int(element[1])
-                    except:
-                        raise UserError(element[1] + ", no parece una cantidad válida. Indica un número entero válido.")
-                    sizes += element[0] + ","
-                    pairs += element[1] + ","
-                    pair_products += str(pppair.id) + ","
-                    pairs_count += int(element[1])
-
-                # OK, guardamos valores, tras quitar la última coma:
-                if len(sizes) > 0: sizes = sizes[:-1]
-                if len(pairs) > 0: pairs = pairs[:-1]
-                if len(pair_products) > 0: pair_products = pair_products[:-1]
-
-                cleanvalues = sizes + ";" + pairs + ";" + pair_products
-                record.write({'assortment_pair': cleanvalues, 'pairs_count': pairs_count})
-    """
