5fc72414e910d71e1879773899e48148507154e2
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2024-09-30 16:04:26 +0200

    sd v17

--- shoes_dealer/models/stock_lot.py
+++ shoes_dealer/models/stock_lot.py
@@ -5,16 +5,8 @@ from odoo import fields, models, api
 class StockLot(models.Model):
     _inherit = "stock.lot"
 
-    # Depende del campo quant_id EN SML:
-    def _get_assortment_pair(self):
-        for record in self:
-            assortment_pair = ""
-            if (record.product_id.is_assortment):
-                assortment_pair = "prueba"
-        record['assortment_pair'] = assortment_pair
-
-    assortment_pair = fields.Char('Assortment pair', compute='_get_assortment_pair')
-
+    # Se cumplimenta desde AA al crear SML:
+    assortment_pair = fields.Char('Assortment pair')
 
     def get_assortment_pair(self):
         for lot in self:
