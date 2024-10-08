171a0871a4fe6cfa71501d0ef9ba7b58ee444981
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2024-10-08 10:39:17 +0200

    shoes shape

--- shoes_dealer/models/shoes_shape.py
+++ shoes_dealer/models/shoes_shape.py
@@ -8,3 +8,14 @@ class ShoesShape(models.Model):
     _description = 'Shoes MRP shape'
 
     name = fields.Char('Name')
+    heel_type = fields.Char('Heel type')
+    heel_height = fields.Float('Heel height')
+    platform_height = fields.Float('Platform height')
+    platform_material_id = fields.Many2one('product.material', string="Platform material")
+    sole_material_main_id = fields.Many2one('product.material', string="Sole material")
+    sole_material_secondary_id = fields.Many2one('product.material', string="Sole material 2")
+    sole_material_main_percent = fields.Float('Sole main mat. (%)')
+    sole_material_secondary_percent = fields.Float('Sole second mat. (%)')
+    insole_material_id = fields.Char('Insole type')
+    insole_material_percent = fields.Float('Insole material (%)')
+    description = fields.Text('Description', translate=True)
