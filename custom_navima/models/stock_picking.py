f69264232ae30418d9f993407d01669ab54e061f
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2024-10-14 15:50:47 +0200

    menu reviews

---
+++ custom_navima/models/stock_picking.py
@@ -0,0 +1,16 @@
+# Copyright
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+
+
+from odoo import fields, models, api
+
+class StockPicking(models.Model):
+    _inherit = 'stock.picking'
+
+    def create_assortment_serial_numbers(self):
+        for record in self:
+            manufacturer =
+            sufix =
+            for li in record.move_ids_without_package:
+                manufacturer_code = li.product_id.manufacturer_id
+        return True
