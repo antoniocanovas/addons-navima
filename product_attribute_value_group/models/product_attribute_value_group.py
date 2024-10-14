82635b42e6d6bf36215ffbc2dfcb7e85d54fa89e
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2024-10-14 12:01:08 +0200

    shoes shape

--- product_attribute_value_group/models/product_attribute_value_group.py
+++ product_attribute_value_group/models/product_attribute_value_group.py
@@ -7,5 +7,5 @@ class ProductAttributeValueGroup(models.Model):
     _name = 'product.attribute.value.group'
     _description = 'Product attribute value Group'
 
-    name = fields.Char('Name')
+    name = fields.Char('Name', translate=True)
     attribute_id = fields.Many2one('product.attribute', string='Attribute')
