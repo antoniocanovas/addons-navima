8ba4793b7c76c95aaea75dc330a3b6744f376190
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2024-10-14 18:29:58 +0200

    create sale lots

--- custom_navima/models/stock_picking.py
+++ custom_navima/models/stock_picking.py
@@ -10,10 +10,21 @@ class StockPicking(models.Model):
     def create_assortment_serial_numbers(self):
         for record in self:
             for li in record.move_ids_without_package:
+                lots = []
                 sufix = li.product_id.campaign_id.name
                 manufacturer_code = li.product_id.manufacturer_id.ref
-                sequence = li.product_id.campaign_id.tracking_sequence
-                serial = sufix + manufacturer_code + sequence
-                # Buscar el serial, si existe sumamos uno:
+                if not sufix or not manufacturer_code:
+                    continue
+
+                for i in li.product_uom_qty:
+                    sequence = str(li.product_id.campaign_id.tracking_sequence + 100000)[-5:]
+
+                    serial = sufix + manufacturer_code + sequence
+                    # Buscar el serial, si existe sumamos uno:
+                    lot = self.env['stock.lot'].search([('product_id','=',li.product_id.id),('name','=',serial)])
+                    if not lot.id:
+                        lot = self.env['stock.lot'].create({'product_id':li.product_id.id, 'name':serial})
+                    lots.append(lot.id)
+                li['lot_ids'] = [(4,lots.ids)]
 
         return True
