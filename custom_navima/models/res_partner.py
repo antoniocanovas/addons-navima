1c44de5126da977ab3534618bf95cdc83410b545
arquero20022 <amm2002ct@gmail.com>
2024-12-05 10:52:59 +0000

    campos mig

--- custom_navima/models/res_partner.py
+++ custom_navima/models/res_partner.py
@@ -17,3 +17,4 @@ class ResPartner(models.Model):
     mig_posicionfiscal = fields.Char('mig_posicionfiscal')
     mig_responsableinterno = fields.Char('mig_posicionfiscal')
     mig_provincia = fields.Char('mig_provincia')
+    mig_vat = fields.Char('mig_vat')
