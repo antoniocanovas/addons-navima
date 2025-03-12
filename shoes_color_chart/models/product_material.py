c514d2549b473b246b7b38d85dd21aaf8042b3f2
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2025-03-12 08:31:05 +0100

    display name material

--- shoes_color_chart/models/product_material.py
+++ shoes_color_chart/models/product_material.py
@@ -9,11 +9,15 @@ class ProductMaterial(models.Model):
 
     code = fields.Char('Code')
     display_name = fields.Char(string='Display name', compute='_compute_display_name', store=True)
+    manufacturer_id = fields.Many2one('res.partner', string='Manufacturer')
+    manufacturer_code = fields.Char(related='manufacturer_id.ref')
 
     @api.depends('name', 'code')
     def _compute_display_name(self):
         for record in self:
-            if record.name and record.code:
+            if record.name and record.code and record.manufacturer_code:
+                record.display_name = f"({record.code})({record.manufacturer_code}) {record.name}"
+            elif record.name and record.code:
                 record.display_name = f"({record.code}) {record.name}"
             elif record.name:
                 record.display_name = record.name
