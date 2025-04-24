252ab4704c806e38d61fdd97b8cb3d41cd584088
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2025-04-24 16:56:20 +0200

    task default code prefix

--- shoes_product_sku/__manifest__.py
+++ shoes_product_sku/__manifest__.py
@@ -39,6 +39,7 @@
         'security/ir.model.access.csv',
         'data/shoes_product_sku_item.xml',
         'views/res_company_views.xml',
+        'views/project_task_views.xml',
         'data/server_actions.xml',
     ],
     "demo": [],
