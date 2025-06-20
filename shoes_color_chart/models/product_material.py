1ac0ea1cdf1c4b0e8a1da6dff9fb44f2eee921da
Adrian Muñoz <amunoz@puntsistemes.es>
2025-06-20 13:11:23 +0200

    [HU74620]: Nuevo campo en partner printed_label_ids

--- shoes_color_chart/models/product_material.py
+++ shoes_color_chart/models/product_material.py
@@ -2,24 +2,37 @@
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 
 
-from odoo import fields, models, api
+from odoo import api, fields, models
+
 
 class ProductMaterial(models.Model):
-    _inherit = 'product.material'
+    _inherit = "product.material"
 
-    code = fields.Char('Code')
-    display_name = fields.Char(string='Display name', compute='_compute_display_name', store=True)
-    manufacturer_id = fields.Many2one('res.partner', string='Manufacturer', ondelete='restrict')
-    manufacturer_code = fields.Char(related='manufacturer_id.ref')
-    material_manufacturer_code = fields.Char('MM code', help='Material & manufacturer concatenated code', store=True,
-                                             compute='_get_material_manufacturer_code')
-    shoes_campaign_ids = fields.Many2many('project.project', string='Campaigns', domain="[('is_shoes_campaign','=',True)]")
+    code = fields.Char("Code")
+    display_name = fields.Char(
+        string="Display name", compute="_compute_display_name", store=True
+    )
+    manufacturer_id = fields.Many2one(
+        "res.partner", string="Manufacturer", ondelete="restrict"
+    )
+    manufacturer_code = fields.Char(related="manufacturer_id.ref")
+    material_manufacturer_code = fields.Char(
+        "MM code",
+        help="Material & manufacturer concatenated code",
+        store=True,
+        compute="_get_material_manufacturer_code",
+    )
+    shoes_campaign_ids = fields.Many2many(
+        "project.project", string="Campaigns", domain="[('is_shoes_campaign','=',True)]"
+    )
 
-    @api.depends('name', 'code', 'manufacturer_id')
+    @api.depends("name", "code", "manufacturer_code")
     def _compute_display_name(self):
         for record in self:
             if record.name and record.code and record.manufacturer_code:
-                record.display_name = f"({record.manufacturer_code}{record.code}) {record.name}"
+                record.display_name = (
+                    f"({record.manufacturer_code}{record.code}) {record.name}"
+                )
             elif record.name and record.code:
                 record.display_name = f"({record.code}) {record.name}"
             elif record.name:
@@ -29,8 +42,7 @@ class ProductMaterial(models.Model):
             else:
                 record.display_name = False
 
-
-    @api.depends('code', 'manufacturer_id.ref')
+    @api.depends("code", "manufacturer_code")
     def _get_material_manufacturer_code(self):
         for record in self:
             code = ""
@@ -38,4 +50,4 @@ class ProductMaterial(models.Model):
                 code += record.code
             if record.manufacturer_code:
                 code += record.manufacturer_code
-            record['material_manufacturer_code'] = code
+            record["material_manufacturer_code"] = code
